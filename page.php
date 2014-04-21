1079d23beb7d1699c00bca93811a6681724b8346
ozh <ozh@ozh.org>
2014-04-21 11:31:41 +0200

    Remove unneeded files

--- page.php
+++ page.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 
+<!-- !BEGIN #tweet -->
+<div id="tweets">
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <div class="post" id="post-<?php the_ID(); ?>">
   <h2><?php the_title(); ?></h2>
@@ -9,6 +12,9 @@
 </div>
 <?php endwhile; endif; ?>
 <?php edit_post_link('edit', '<p>', '</p>'); ?>
+
+</div>
+<!-- END #tweet -->
   
   
 <?php get_footer(); ?>
